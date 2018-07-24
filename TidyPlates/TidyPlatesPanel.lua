91d442333782930b1e576ab8b36144370178f575
Luxocracy <luxor355@gmail.com>
2018-07-24 19:07:00 +0200

    Fixed window resize freezing the game client
    - Fixed an issue causing the game client to freeze when resizing the
    window in windowed mode.

--- TidyPlates/TidyPlatesPanel.lua
+++ TidyPlates/TidyPlatesPanel.lua
@@ -75,8 +75,22 @@ end
 TidyPlates.GetProfile = GetProfile
 
 
-function TidyPlatesPanel.AddProfile(self, profileName )
-	if  profileName then
+function TidyPlatesPanel.ProfileExists(self, profileName)
+	if profileName then
+		local exists = true
+		for i=1, #HubProfileList do
+			if profileName == HubProfileList[i].text then
+				exists = true
+				break
+			end
+		end
+		return exists
+	end
+end
+
+
+function TidyPlatesPanel.AddProfile(self, profileName)
+	if profileName then
 		HubProfileList[#HubProfileList+1] = { text = profileName, value = profileName, }
 	end
 end
