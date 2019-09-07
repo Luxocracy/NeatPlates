40aaaecb2dd4de112fc6991d702cf2dbc4da8564
Luxocracy <luxor355@gmail.com>
2019-09-08 00:08:37 +0200

    Changed how client version is detected
    - Changed how client version is detected

--- NeatPlatesHub/Hub.lua
+++ NeatPlatesHub/Hub.lua
@@ -821,7 +821,7 @@ HubHandler:SetScript("OnEvent", function(...)
 	local TPCHubEnabled = GetAddOnEnableState(player, "TidyPlatesContinuedHub") ~= 0
 
 	-- Determine which client we are on (Retail or Classic)
-	if tonumber(strmatch(version, "[0-9]")) < 8 then VersionWarning() end
+	if _G.WOW_PROJECT_ID == _G.WOW_PROJECT_CLASSIC then VersionWarning() end
 
 	if addon == "NeatPlatesHub" and (not TPCEnabled or not TPCHubEnabled) then
 		LoadProfiles(NeatPlatesHubSettings.profiles)
