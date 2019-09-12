bc2477e17e92fa9cec93a668d447646828b2373f
Luxocracy <luxor355@gmail.com>
2019-09-12 22:35:42 +0200

    Update Core.lua
    - More fixes to frame width scaling

--- NeatPlatesHub/functions/Core.lua
+++ NeatPlatesHub/functions/Core.lua
@@ -257,16 +257,6 @@ local function ApplyCustomBarSize(style, defaults)
 			if defaults[v].width then style[v].width = defaults[v].width * (LocalVars.CastBarWidth or 1) end
 			if defaults[v].x then style[v].x = defaults[v].x * (LocalVars.CastBarWidth or 1) end
 		end
-		--style.threatborder.width = defaults.threatborder.width * (LocalVars.FrameBarWidth or 1)
-		--style.healthborder.width = defaults.healthborder.width * (LocalVars.FrameBarWidth or 1)
-		--style.healthbar.width = defaults.healthbar.width * (LocalVars.FrameBarWidth or 1)
-		--style.frame.width = defaults.frame.width * (LocalVars.FrameBarWidth or 1)
-		--style.customtext.width = defaults.customtext.width * (LocalVars.FrameBarWidth or 1)
-		--style.level.width = defaults.level.width * (LocalVars.FrameBarWidth or 1)
-		--style.name.width = defaults.name.width * (LocalVars.FrameBarWidth or 1)
-		--style.healthbar.x = defaults.healthbar.x * (LocalVars.FrameBarWidth or 1)
-		--style.name.x = defaults.name.x * (LocalVars.FrameBarWidth or 1)
-		--style.level.x = defaults.level.x * (LocalVars.FrameBarWidth or 1)
 
 		
 		-- Castbar
@@ -275,18 +265,10 @@ local function ApplyCustomBarSize(style, defaults)
 			if defaults[v].width then style[v].width = defaults[v].width * (LocalVars.CastBarWidth or 1) end
 			if defaults[v].x then style[v].x = defaults[v].x * (LocalVars.CastBarWidth or 1) end
 		end
-		--style.castborder.width = defaults.castborder.width * (LocalVars.CastBarWidth or 1)
-		--style.castnostop.width = defaults.castnostop.width * (LocalVars.CastBarWidth or 1)
-		--style.castbar.width = defaults.castbar.width * (LocalVars.CastBarWidth or 1)
-		--style.castborder.x = defaults.castborder.x * (LocalVars.CastBarWidth or 1)
-		--style.castnostop.x = defaults.castnostop.x * (LocalVars.CastBarWidth or 1)
-		--style.castbar.x = defaults.castbar.x * (LocalVars.CastBarWidth or 1)
-		--style.spellicon.x = defaults.spellicon.x * (LocalVars.CastBarWidth or 1)
-		--style.spelltext.x = defaults.spelltext.x * (LocalVars.CastBarWidth or 1)
-		--style.durationtext.x = defaults.durationtext.x * (LocalVars.CastBarWidth or 1)
 
 		-- Things we don't want to apply width to
 		style.eliteicon.x = defaults.eliteicon.x * (LocalVars.FrameBarWidth or 1)
+		if style.eliteicon.width > 64 then style.eliteicon.width = defaults.eliteicon.width * (LocalVars.FrameBarWidth or 1) end
 		
 	
 		-- Defined elsewhere so they need to be handled differently
