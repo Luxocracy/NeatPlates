1bca4a02bca626f9e0038753591d6b6400fbdfee
Luxocracy <luxor355@gmail.com>
2019-10-31 21:32:25 +0100

    Tank assign fix & lib update
    - Fixed manual tank assignment overriding raid tank assignment
    - Updated 'LibClassicDurations'

--- NeatPlates/libs/LibClassicDurations/classAbilities.lua
+++ NeatPlates/libs/LibClassicDurations/classAbilities.lua
@@ -1,7 +1,7 @@
 local lib = LibStub and LibStub("LibClassicDurations", true)
 if not lib then return end
 
-local Type, Version = "SpellTable", 39
+local Type, Version = "SpellTable", 40
 if lib:GetDataVersion(Type) >= Version then return end  -- older versions didn't have that function
 
 local Spell = lib.AddAura
