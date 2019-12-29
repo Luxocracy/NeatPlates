26a8259c25885c7500742d3cc07ec244ca2ab1ae
Luxocracy <luxor355@gmail.com>
2019-12-29 11:36:45 +0100

    Revert "Merge branch 'classic-theme_customization'"
    This reverts commit 6da1a60e1a00af6eacec25054bdc59476b1ed7f3.

--- NeatPlatesHub/Helpers.lua
+++ NeatPlatesHub/Helpers.lua
@@ -12,8 +12,9 @@ local function CallForStyleUpdate()
 	local theme = NeatPlates:GetTheme()
 	--print("CallForStyleUpdate, Theme,", theme)
 
-	if theme.ApplyProfileSettings then theme:ApplyProfileSettings("From CallForStyleUpdate") end
-	if theme.ApplyThemeCustomization then theme:ApplyThemeCustomization() end
+	if theme.ApplyProfileSettings
+		then theme:ApplyProfileSettings("From CallForStyleUpdate")
+	end
 
 end
 
