7e72e931959fb096367f3ef4791ace71f2963dbc
Luxocracy <luxor355@gmail.com>
2020-05-03 19:55:19 +0200

    Changed when blizz nameplates are hidden
    - Changed when blizzard nameplates get hidden because it was getting called every frame, which I believe was causing issues sometimes.

--- NeatPlates/NeatPlatesCore.lua
+++ NeatPlates/NeatPlatesCore.lua
@@ -326,9 +326,6 @@ do
 				plate.UpdateMe = false
 				plate.UpdateHealth = false
 
-				local children = plate:GetChildren()
-				if children then children:Hide() end
-
 				if plate.UpdateCastbar then -- Check if spell is being cast
 					local unitGUID = UnitGUID(unit.unitid)
 					if unitGUID and SpellCastCache[unitGUID] and not SpellCastCache[unitGUID].finished then OnStartCasting(plate, unitGUID, false)
@@ -338,8 +335,6 @@ do
 			elseif unitid and not plate:IsVisible() then
 				OnHideNameplate(plate, unitid)  -- If the 'NAME_PLATE_UNIT_REMOVED' event didn't trigger
 			end
-
-			if plate.UnitFrame then plate.UnitFrame:Hide() end
 
 		-- This would be useful for alpha fades
 		-- But right now it's just going to get set directly
@@ -573,6 +568,7 @@ do
 		-- or unitid = plate.namePlateUnitToken
 		UpdateReferences(plate)
 
+		plate.UnitFrame:Hide()	-- Hide Blizzard Nameplates
 		carrier:Show()
 
 		PlatesVisible[plate] = unitid
@@ -624,6 +620,7 @@ do
 	function OnHideNameplate(plate, unitid)
 		local unitGUID = UnitGUID(unitid)
 		--plate.extended:Hide()
+		plate.UnitFrame:Hide()	-- Hide Blizzard Nameplates
 		plate.carrier:Hide()
 
 		UpdateReferences(plate)
@@ -1339,11 +1336,7 @@ do
 		
 		-- Ignore if plate is Personal Display
 		if plate and not UnitIsUnit("player", unitid) then
-			local children = plate:GetChildren()
-			if children then children:Hide() end --Avoids errors incase the plate has no children
-
 			if NeatPlatesTarget and unitid and UnitGUID(unitid) == NeatPlatesTarget.unitGUID then toggleNeatPlatesTarget(false) end
-
 	 		OnShowNameplate(plate, unitid)
 	 	end
 	end
@@ -1411,6 +1404,12 @@ do
 	end
 
 	function CoreEvents:DISPLAY_SIZE_CHANGED()
+		ForEachPlate(function(plate) plate.UnitFrame:Hide() end)
+		SetUpdateAll()
+	end
+
+	function CoreEvents:UI_SCALE_CHANGED()
+		ForEachPlate(function(plate) plate.UnitFrame:Hide() end)
 		SetUpdateAll()
 	end
 
@@ -1836,7 +1835,10 @@ function NeatPlates:ShowNameplateSize(show, width, height) ForEachPlate(function
 
 function NeatPlates:ForceUpdate() ForEachPlate(OnResetNameplate) end
 function NeatPlates:ResetWidgets() ForEachPlate(OnResetWidgets) end
-function NeatPlates:Update() SetUpdateAll() end
+function NeatPlates:Update(...)
+	ForEachPlate(function(plate) plate.UnitFrame:Hide() end)
+	SetUpdateAll()
+end
 
 function NeatPlates:RequestUpdate(plate) if plate then SetUpdateMe(plate) else SetUpdateAll() end end
 
