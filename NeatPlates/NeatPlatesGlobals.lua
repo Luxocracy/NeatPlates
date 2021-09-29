e6df9e6da16972e84b4e894126ebeb593195dca9
Luxocracy <Luxocracy@users.noreply.github.com>
2021-09-29 23:06:49 +0200

    remove debug version print
    - remove debug version print

--- NeatPlates/NeatPlatesGlobals.lua
+++ NeatPlates/NeatPlatesGlobals.lua
@@ -2,7 +2,6 @@ NEATPLATES_IS_CLASSIC_TBC = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSI
 NEATPLATES_IS_CLASSIC_ERA = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
 NEATPLATES_IS_CLASSIC = NEATPLATES_IS_CLASSIC_TBC or NEATPLATES_IS_CLASSIC_ERA
 local wowversion, wowbuild, wowdate, wowtocversion = GetBuildInfo()
-print(wowtocversion)
 if wowtocversion and (wowtocversion > 90000 or (NEATPLATES_IS_CLASSIC and wowtocversion >= 11400)) then
 	NeatPlatesBackdrop = "BackdropTemplate"
 end
